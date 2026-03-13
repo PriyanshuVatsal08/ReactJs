64021003991ab2c5b6946c4e46a23c67f6e2a3f0
PriyanshuVatsal08 <priyanshuvatsal2572004@gmail.com>
2026-03-13 22:48:25 +0530

    Basic of react

---
+++ my-first-react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const name="Priyanshu Vatsal";
+
+  return(
+    <div>
+      <h1>Hello {name}</h1>
+      <p>Welcome to ReactJS</p>
+    </div>
+  )
+}
+
+export default App
---
+++ my-first-react/src/Component/Navbar.jsx
@@ -0,0 +1,12 @@
+// rafce
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h3>I am navbar </h3>
+    </div>
+  )
+}
+
+export default Navbar
