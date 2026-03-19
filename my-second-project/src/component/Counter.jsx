c9a62d4ae766519384f7f0e7eb6159b5204c53f4
PriyanshuVatsal08 <priyanshuvatsal2572004@gmail.com>
2026-03-19 18:19:05 +0530

    All code of my-second-project push

--- my-second-project/src/component/Counter.jsx
+++ my-second-project/src/component/Counter.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import './Counter.css'
 import { useState } from "react";
 const Counter = () => {
@@ -6,10 +6,14 @@ const Counter = () => {
   const[count,setCount]=useState(0);
   return (
     <div className='counter-body'>
-      <p>Number of time you click {count}</p>
-      <button onClick={()=>setCount(count+1)}>Click me</button>
+      <p>Number of time you click "{count}" </p>
+      <div className='button-div'>
+        <button onClick={()=>setCount(count+1)} >Add</button>
 
-      <button onClick={()=>setCount(0)}>Reset</button>
+        <button onClick={()=>setCount(count>0 ? count-1 : 0)}>Minus</button>
+
+        <button onClick={()=>setCount(0)}>Reset</button>
+      </div>
     </div>
   )
 }
